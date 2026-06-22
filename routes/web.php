e743bb89aa244a00324e038061e5d69c1010839c
Muhamad Akmal <mhmdakmal46@gmail.com>
2026-06-22 23:12:17 +0700

    remove info

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/debug-server', function (Request $request) {
-    return response()->json([
-        'server_software' => $_SERVER['SERVER_SOFTWARE'] ?? 'not set',
-        'server_name' => $_SERVER['SERVER_NAME'] ?? 'not set',
-        'server_port' => $_SERVER['SERVER_PORT'] ?? 'not set',
-        'php_version' => PHP_VERSION,
-        'php_sapi' => PHP_SAPI,
-        'laravel_version' => app()->version(),
-    ]);
 });
